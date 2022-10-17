7f56d01a3591242d795e6b0eff7e631cee8a73ba
karan borkar <karan borkar@ROG>
2022-10-17 13:32:19 +0530

    first java

---
+++ First Java Project/src/FirstPackage/Classs2.java
@@ -0,0 +1,24 @@
+package FirstPackage;
+
+public class Classs2 {
+public static void main(String[] args) {
+	
+	
+	for (int i=1;i<=5;i++) {
+
+		for (int j=1;j<=i;j++) {
+			System.out.print("*");
+		}
+		System.out.println();
+	}
+	System.out.println("____________________");
+	
+	for (int i=1;i<=5;i++) {
+
+		for (int j=i;j<=5;j++) {
+			System.out.print("*");
+		}
+		System.out.println();
+	}
+}
+}
---
+++ First Java Project/src/FirstPackage/class1.java
@@ -0,0 +1,25 @@
+package FirstPackage;
+
+public class class1 {
+	public static void main(String[] args) {
+
+		for(int a=1;a<=5;a++) {
+			
+			for(int b=1;b<=a ;b++) {
+				System.out.print("*");
+			}
+			
+			System.out.println();
+		}
+		System.out.println("______________________");
+		System.out.println();
+		for(int i=1;i<=5;i++) {
+			for(int j=i;j<=5;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+	}
+
+}
